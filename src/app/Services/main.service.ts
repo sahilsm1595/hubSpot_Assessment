1c6da5df408ab054a7d68dfac0f77b6379415b60
Sahil Sachin More <sahil.more@steinnlabs.com>
2021-10-24 13:19:39 +0100

    Exercise 2 functionality done

--- src/app/Services/main.service.ts
+++ src/app/Services/main.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 
 @Injectable({
@@ -7,11 +8,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MainService {
 
-  movieListUrl = '../../assets/data/data.json';
+  // movieListUrl = '../../assets/data/data.json';
+  movieListUrl = 'https://raw.githubusercontent.com/HubSpotWebTeam/CodeExercise/main/src/js/data/data.json';
+
+
+  private movieList = new Subject();
+  _movieList = this.movieList.asObservable();
 
   constructor(private http: HttpClient) { }
 
   getMovieListItems() {
     return this.http.get(this.movieListUrl);
+  }
+
+  setMovieList(movieItems) {
+    this.movieList.next(movieItems);
   }
 }
