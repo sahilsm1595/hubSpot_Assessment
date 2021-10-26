19b9598ba2f2fafa3eba6e696009ace394f8c873
Sahil Sachin More <sahil.more@steinnlabs.com>
2021-10-26 03:48:22 +0100

    Exercise 2 done

--- src/app/exercise2/exercise2.component.ts
+++ src/app/exercise2/exercise2.component.ts
@@ -1,224 +1,161 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MainService } from '../Services/main.service';
+import { Subscription } from 'rxjs';
+
+interface IMovie {
+  title: string;
+  year: string;
+  poster: string;
+  genre: [string];
+  type: string;
+}
+
+interface IYear {
+  name: string;
+  checked: boolean;
+}
+
+interface IGenre {
+  name: string;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-exercise2',
   templateUrl: './exercise2.component.html',
-  styleUrls: ['./exercise2.component.scss']
+  styleUrls: ['./exercise2.component.scss'],
 })
-export class Exercise2Component implements OnInit {
+export class Exercise2Component implements OnInit, OnDestroy {
+  movieSubscription$: Subscription;
+  filteredMovieList: IMovie[];
+  data: any = [];
 
-  movieListItems: any;
-  genreList: any;
-  tempList: any;
-  yearList: any;
-  selectedYear = '';
-
-  dropdownList = [];
-  selectedItems = [];
-  dropdownSettings = {};
-
-  dropdownListNew: any;
+  yearList: IYear[];
   genreArr: any;
-  filteredGenreArr: any;
+  genreList: any;
+  dropdownGenre: string;
+  dropdownYear: string;
 
+  movieTitle: string;
+  radioValue: string;
 
-  radioArr: any;
-  movieData: any = [];
-  movieTitle: any;
-  radioFlag: boolean;
+  itemYears: any[];
+  itemGenres: any[];
 
   constructor(private mainService: MainService) {
-    this.movieListItems = [];
-    this.genreList = [];
-    this.tempList = [];
-    this.radioArr = [];
-    this.yearList = [];
-    this.radioFlag = false;
-    this.dropdownListNew = [];
     this.getMovieListItems();
     this.mainService._movieList.subscribe((res: any) => {
-      this.movieListItems = res;
+      this.filteredMovieList = res;
     });
   }
 
   ngOnInit() {
-    this.movieListItems = [];
-    this.tempList = [
-      { id: 1, name: 'Sahil' },
-      { id: 2, name: 'Mangesh' },
-      { id: 3, name: 'Somnath' },
-      { id: 4, name: 'Sutar' }
-    ];
-
-    this.dropdownList = [
-      { id: 1, itemName: 'action' },
-      { id: 2, itemName: 'adventure' },
-      { id: 3, itemName: 'comedy' },
-      { id: 4, itemName: 'sci-fi' },
-      { id: 5, itemName: 'thriller' },
-      { id: 6, itemName: 'fantasy' },
-      { id: 7, itemName: 'western' },
-      { id: 8, itemName: 'drama' },
-      { id: 9, itemName: 'mystery' },
-      { id: 10, itemName: 'crime' },
-      { id: 12, itemName: 'horror' },
-      { id: 12, itemName: 'animation' },
-      { id: 13, itemName: 'war' },
-      { id: 14, itemName: 'biography' }
-
-    ];
-    // this.selectedItems = [
-    //   { id: 2, itemName: 'Singapore' },
-    //   { id: 3, itemName: 'Australia' },
-    //   { id: 4, itemName: 'Canada' },
-    //   { id: 5, itemName: 'South Korea' }
-    // ];
-    this.dropdownSettings = {
-      singleSelection: false,
-      text: 'Generes',
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      enableSearchFilter: false,
-      classes: 'myclass custom-class select-dropdown'
-    };
+    this.mainService._movieList.subscribe((res: any) => {
+      this.filteredMovieList = res;
+    });
+    this.init();
+  }
+  init() {
+    this.data = [];
+    this.dropdownGenre = 'genre';
+    this.dropdownYear = 'year';
+    this.movieTitle = '';
+    this.radioValue = '';
+    this.itemYears = [];
+    this.itemGenres = [];
+    // this.globalFIlters = {};
+    this.filteredMovieList = this.data;
+    this.yearList = [];
+    this.genreArr = [];
+    this.genreList = [];
   }
 
   getMovieListItems() {
-    this.genreArr = [];
-    this.filteredGenreArr = [];
-
-    this.mainService.getMovieListItems().subscribe((res: any) => {
-      // this.movieListItems = res.media;
-      this.movieData = res.media;
-
-      // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < this.movieData.length; i++) {
-        // tslint:disable-next-line:prefer-for-of
-        for (let j = 0; j < this.movieData[i].genre.length; j++) {
-          this.genreArr.push({
-            itemName: this.movieData[i].genre[j]
-            // id: i
-          });
-        }
-      }
-      this.filteredGenreArr = this.genreArr.reduce((filteredGenreArr, item) => {
-        if (!filteredGenreArr.some(filteredItem => JSON.stringify(filteredItem) === JSON.stringify(item))) {
-          filteredGenreArr.push(item);
-        }
-        return filteredGenreArr;
-      }, []);
-
-      this.yearList = this.movieData.map((item) => {
-        // return empIds.indexOf(itm.empid) > -1;
-        return item.year;
+    this.movieSubscription$ = this.mainService.getMovieListItems().subscribe((res: any) => {
+      this.data = res.media;
+      // **** Dropdown GENRE data ****
+      const filterData = new Set(this.data.map((a) => a.genre).flat());
+      this.genreList = [...filterData].map((e) => {
+        return {
+          name: e,
+          checked: false,
+        };
       });
+      // **** Dropdown YEAR data****
+      this.yearList = this.data.map((item) => {
+        return { name: item.year, checked: false };
+      });
+      // removed duplicate years
+      this.yearList = this.yearList.filter(
+        (item, index, self) => index === self.findIndex((t) => t.name === item.name)
+      );
+      this.yearList.sort((a, b) => (a.name < b.name ? 1 : b.name < a.name ? -1 : 0));
 
-      console.log('year', this.yearList);
-
-
-      // this.dropdownListNew = this.movieData.filter((item) => {
-      //   // return empIds.indexOf(itm.empid) > -1;
-      //   if (item.genre.length > 0) {
-      //     // console.log(item.genre);
-      //     // this.genreArr.push({
-      //     //   genre: item.genre
-      //     // });
-
-      //     // this.genreArr.push({ genre: item.genre });
-
-      //     this.genreArr.Genre = item.genre;
-
-      //     console.log('**', this.genreArr);
-      //   }
-      // });
-
-
-      // console.log('@@', this.dropdownListNew);
-
-      this.mainService.setMovieList(this.movieData);
+      this.setData(this.data);
     });
-
   }
 
-  // RADIO BUTTONS
-  onRadioChange(value) {
-    console.log('Value is : ', value);
-    if (value === '') {
-      this.radioFlag = false;
+  // Radio Buttons
+  onTypeChange(value) {
+    this.radioValue = value;
+    this.applyFilters();
+  }
+
+  applyFilters() {
+    this.filteredMovieList = this.data.filter(
+      (item) =>
+        (this.itemYears.length === 0 || this.itemYears.includes(item.year)) &&
+        (this.radioValue === '' || item.type === this.radioValue) &&
+        (this.itemGenres.length === 0 || item.genre.some((elem) => this.itemGenres.some((d) => d === elem)))
+    );
+    this.setData(this.filteredMovieList);
+  }
+  onSearch(): void {
+    if (this.movieTitle) {
+      if (this.radioValue) {
+        this.filteredMovieList = this.filteredMovieList.filter((item) => {
+          const result = item.title.toLocaleLowerCase().match(this.movieTitle.toLocaleLowerCase());
+          return result;
+        });
+      } else {
+        this.filteredMovieList = this.data.filter((item) => {
+          const result = item.title.toLocaleLowerCase().match(this.movieTitle.toLocaleLowerCase());
+          return result;
+        });
+      }
     } else {
-      this.radioFlag = true;
-      this.movieListItems = this.movieData.filter((item) => {
-        return item.type === value;
-      });
-      console.log(this.movieListItems);
-      this.mainService.setMovieList(this.movieListItems);
+      if (this.radioValue) {
+        this.onTypeChange(this.radioValue);
+      } else {
+        this.getMovieListItems();
+      }
     }
   }
 
-
-  // SEARCH
-  search() {
-    if (this.movieTitle === '') {
-      this.getMovieListItems();
-    } else {
-      this.movieListItems = this.movieData.filter((item) => {
-        return item.title.toLocaleLowerCase().match(this.movieTitle.toLocaleLowerCase());
-      });
-      console.log('CURRENT ALL ->', this.movieListItems);
-      // this.mainService.setMovieList(this.movieData);
-
-
-      // // Radio btn search issue
-      // if (this.radioFlag === true) {
-      //   // ON FILTERD DATA (Movies/Books)
-      //   this.movieListItems = this.movieListItems.filter((item) => {
-      //     return item.title.toLocaleLowerCase().match(this.movieTitle.toLocaleLowerCase());
-      //   });
-      //   console.log('ON FILTERD DATA ->', this.movieListItems);
-      // } else {
-      //   // ON ALL DATA
-      //   this.movieListItems = this.movieData.filter((item) => {
-      //     return item.title.toLocaleLowerCase().match(this.movieTitle.toLocaleLowerCase());
-      //   });
-      //   console.log('CURRENT ALL ->', this.movieListItems);
-      //   // this.mainService.setMovieList(this.movieData);
-      // }
-
-    }
+  // Dropdown Checked GENRE list
+  filterByGenre(itemGenres: any[]) {
+    this.itemGenres = itemGenres;
+    this.applyFilters();
   }
-
-
-  // YEAR DROPDOWN
-  selectChangeHandler(event: any) {
-    this.selectedYear = event.target.value;
-    console.log(this.selectedYear);
-    if (this.selectedYear === '') {
-    } else {
-      this.movieListItems = this.movieData.filter((item) => {
-        return item.year === this.selectedYear;
-      });
-      console.log(this.movieListItems);
-      this.mainService.setMovieList(this.movieListItems);
-    }
-  }
-
-
-
-  // DROPDOWN FUNCTIONS
-  onItemSelect(item: any) {
+  filterByGenreIndividual(item: {}) {
     console.log(item);
-    console.log(this.selectedItems);
-  }
-  OnItemDeSelect(item: any) {
-    console.log(item);
-    console.log(this.selectedItems);
-  }
-  onSelectAll(items: any) {
-    console.log(items);
-  }
-  onDeSelectAll(items: any) {
-    console.log(items);
   }
 
+  // Dropdown Checked YEAR list
+  filterByYear(itemYears: any[]) {
+    this.itemYears = itemYears;
+    this.applyFilters();
+  }
+
+  filterByYearIndividual(item: {}) {}
+
+  setData(data): void {
+    this.mainService.setMovieList(data);
+  }
+
+  ngOnDestroy() {
+    if (this.movieSubscription$) {
+      this.movieSubscription$.unsubscribe();
+    }
+  }
 }
